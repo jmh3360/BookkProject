b84167e05734e8c891941a578c84d03d80d0ebf9
1027 <1027@hb1021>
2018-04-17 13:01:49 +0900

    end

--- src/main/webapp/resources/js/view.js
+++ src/main/webapp/resources/js/view.js
@@ -1,6 +1,13 @@
 /**
  * 
  */
+var createMultiDiv=x=>{
+	var temp = '';
+	$.each(x.arr,(i,j)=>{
+		temp+='<div id ="'+x.id+'-'+i+'"></div>'
+	});
+	return temp;
+}
 var createViewTr=x=>{
 	var tmep ='';
 	
@@ -28,6 +35,13 @@ var createMultiTr=x=>{
 	var temp = '';
 	$.each(x.arr,(i,j)=>{
 		temp+='<tr id ="'+x.id+'-'+i+'"></tr>'
+	});
+	return temp;
+}
+var createMultiTd=x=>{
+	var temp = '';
+	$.each(x.arr,(i,j)=>{
+		temp+='<td id ="'+x.id+'-'+i+'"></td>'
 	});
 	return temp;
 }
@@ -86,15 +100,15 @@ var createTd = x=>{
 	return '<td id="'+x.id+'" class="'+x.clazz+'"></td>'
 }
 //순서가 상관없는 list를 짤 때 씀..
-var createUl = x=>{
+var createUL = x=>{
 	return '<ul id="'+x.id+'" class="'+x.clazz+'"></ul>'
 }
 //ol은 type에 알파벳 l을 넣어주면 로마숫자로 표기되어 순서대로 나열
 //또는 start="5"라고 넣어주면 입력된 li값 앞에 5라고 표기되면서 순차적으로 나열
-var createOL=x=>{
+var createOl=x=>{
     return '<ol id="'+x.id+'"class="'+x.clazz+'"></ol>';
 }
-var createLi = x=>{
+var createLI = x=>{
 	return '<li id = "li-'+x.id+'" class ="'+x.clazz+'"></li>'
 }
 var createLabel=x=>{
@@ -107,7 +121,7 @@ var createATag=x=>{
 	return '<a id ="'+x.id+'" href="#"> '+x.val+'</a>';
 }
 var createSpan=x=>{
-	return '<span id="span-'+x.id+'" class="'+x.clazz+'"></span>';
+	return '<span id="'+x.id+'" class="'+x.clazz+'"></span>';
 }
 var createHTag=x=>{
 	return '<h'+x.size+'>'+x.val+'</h'+x.size+'>';
